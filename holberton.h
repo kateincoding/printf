27210d41ece55bd0363820158516bc64a5bb88cc
Your Name <k.soto@pucp.pe>
2021-03-13 18:56:57 -0500

    creating structure of %op with respective functions to call in printf

--- holberton.h
+++ holberton.h
@@ -1,6 +1,24 @@
 #ifndef HOLBERTON_H
 # define HOLBERTON_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct Ftype - Struct operator
+ *
+ * @c: Parameter char (%s, %c, %i, ...)
+ * @f: The function associated
+ */
+
+typedef struct Ftype
+{
+	char *c;
+	void (*f)(va_list);
+} ftype;
+
+typedef struct Ftype f_type;
+
 int _printf(const char *format, ...);
 
 #endif
