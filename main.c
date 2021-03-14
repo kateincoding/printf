06e30e60a3e5978163c022ad2234476f2001f011
tavolafourcade <tavolafourcade@gmail.com>
2021-03-14 17:18:51 -0500

    MAde changes over the print_str function

--- main.c
+++ main.c
@@ -1,1 +1,8 @@
-int print_percent(va_list lista)
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void)
+{
+  print_str([ "h", "o", "l", "a" ]);
+}
--- operator_printf.c
+++ operator_printf.c
@@ -11,10 +11,8 @@
 int operator_printf(char s_type, va_list list)
 {
     f_type operator[] = {
-		{'c', f_char},
+        {'c', print_char},
         {'s', f_string},
-        {'d', f_decimal}
-		{'i', f_int},
-        {'\0', 0}
-	};
+        {'d', f_decimal} {'i', f_int},
+        {'\0', 0}};
 }
