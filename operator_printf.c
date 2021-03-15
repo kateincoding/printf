757dc3f94a4b027ecd0c75eb687cd655dd0e6c9e
Your Name <k.soto@pucp.pe>
2021-03-14 19:17:34 -0500

    final

--- operator_printf.c
+++ operator_printf.c
@@ -13,14 +13,8 @@ int operator_printf(char s_type, va_list list)
     f_type operator[] = {
         {'c', print_char},
         {'s', f_string},
-<<<<<<< HEAD
         {'d', f_decimal}
 		{'i', f_int},
         {'\0', 0}
 	};
-}   
-=======
-        {'d', f_decimal} {'i', f_int},
-        {'\0', 0}};
 }
->>>>>>> b9b36b4bb2f6029e917cdef05467074e0dc83984
