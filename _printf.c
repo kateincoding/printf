38438c48bba6ea8f16e93d2aa23af6aca2549bbb
Your Name <k.soto@pucp.pe>
2021-03-14 19:07:16 -0500

    changing printf fx

--- _printf.c
+++ _printf.c
@@ -7,21 +7,26 @@
  */
 int _printf(const char *format, ...)
 {
-    unsigned int i,
+    unsigned int i = 0, result = 0;
     va_list list;
 
     va_start(list, format);
     
-    i = 0;
     while (format && format[i])
 	{
         if (format[i] != '%')
-            _putchar(format[i]);
+            result += _putchar(format[i]);
 		else
+		{
+			format++;
+			result += operator_printf(format[++i], list);
+		}
 			/*functions that search in structured list the operator and returns the function */
 			/*step2: call the value with write(0) from fx and returned*/
         i++;
     }
+	va_end(list);
+	return (result);
 }
 
 /*
