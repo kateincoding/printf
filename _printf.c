681c86a055b354e7d106cc4b59c92ca8fc8875e8
Your Name <k.soto@pucp.pe>
2021-03-14 21:51:46 -0500

    main printf fx finished

--- _printf.c
+++ _printf.c
@@ -18,7 +18,6 @@ int _printf(const char *format, ...)
             result += _putchar(format[i]);
 		else
 		{
-			format++;
 			result += operator_printf(format[++i], list);
 		}
 			/*functions that search in structured list the operator and returns the function */
@@ -28,37 +27,3 @@ int _printf(const char *format, ...)
 	va_end(list);
 	return (result);
 }
-
-/*
-se replicara algo como el ejercicio 3 del ul project
-cuando el i encuentre %c o %(x)
-ftype op[] = {
-		{"c", f_char},
-		{"i", f_int},
-		{"f", f_float},
-		{"s", f_string}
-	};
-
-	va_list args;
-	char *separators = "";
-	unsigned int i = 0, j = 0;
-
-	va_start(args, format);
-	while (format && format[i])
-	{
-		j = 0;
-		while (j < 4)
-		{
-			if (format[i] == op[j].c[0])
-			{
-				printf("%s", separators);
-				op[j].f(args);
-				separators = ", ";
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("\n");
-	va_end(args);
-    */
