7918ca1c95fa5c487c71f4709741be83befd39c7
tavolafourcade <tavolafourcade@gmail.com>
2021-03-16 16:59:05 -0500

    Incorporated the ft_atoi function

---
+++ ft_atoi.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *ft_atoi - extract the number to integer
+ *@str: string input
+ *Return: number int
+ */
+int ft_atoi(const char *str)
+{
+  int result = 0;
+  int sign = 1;
+  int digit;
+  if (*str == '-')
+  {
+    sign = -1;
+    ++str;
+  }
+  while (*str >= '0' && *str <= '9')
+  {
+    result = result * 10;
+    digit = *str - '0';
+    result = result + (digit * sign);
+    ++str;
+  }
+  return (result);
+}
