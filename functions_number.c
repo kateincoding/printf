e8c7ca7af44baab5925d3bfa11b84e72c83341eb
tavolafourcade <tavolafourcade@gmail.com>
2021-03-15 23:57:09 -0500

    Changed position of octal to a new file dedicated to the different bases

--- functions_number.c
+++ functions_number.c
@@ -72,37 +72,3 @@ int print_unsigned(va_list lista)
   }
   return (c);
 }
-
-/**
- * print_octal - Converts a decimal num passed to the argument to an octal
- * num
- * @lista: The num to be converted
- * Return: c of digit in octal num
- */
-int print_octal(va_list lista)
-{
-  unsigned int num, c = 0, index = 0;
-  int arr[100];
-
-  num = va_arg(lista, int);
-  if (num < 9)
-  {
-    _putchar(num + '0');
-    c = 1;
-  }
-  else if (num >= 9)
-  {
-    while (num > 0)
-    {
-      arr[index] = num % 8;
-      num /= 8;
-      index++;
-    }
-  }
-  while (index--)
-  {
-    _putchar(arr[index] + '0');
-    c++;
-  }
-  return (c);
-}
