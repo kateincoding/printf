561e14de33bb028c93fd3934b654ae0fcfb1507e
Your Name <k.soto@pucp.pe>
2021-03-16 02:08:53 -0500

    passing betty style

--- function_binary.c
+++ function_binary.c
@@ -1,33 +1,34 @@
 #include "holberton.h"
 
 /**
-* print_binary - Converts unsigned int argument to binary
+* print_bin - Converts unsigned int argument to binary
 * @lista: The decimal number to be converted
 * Return: Binary number
 */
 int print_bin(va_list lista)
 {
-  unsigned int n, a = 0, i = 0;
-  int arr[100];
-  n = va_arg(lista, int);
-  if (n < 2)
-  {
-    _putchar(n + '0');
-    a = 1;
-  }
-  else if (n >= 2)
-  {
-    while (n > 0)
-    {
-      arr[i] = n % 2;
-      n /= 2;
-      i++;
-    }
-  }
-  while (i--)
-  {
-    _putchar(arr[i] + '0');
-    a++;
-  }
-  return (a);
+	unsigned int n, a = 0, i = 0;
+	int arr[100];
+
+	n = va_arg(lista, int);
+	if (n < 2)
+	{
+		_putchar(n + '0');
+		a = 1;
+	}
+	else if (n >= 2)
+	{
+		while (n > 0)
+		{
+			arr[i] = n % 2;
+			n /= 2;
+			i++;
+		}
+	}
+	while (i--)
+	{
+		_putchar(arr[i] + '0');
+		a++;
+	}
+	return (a);
 }
